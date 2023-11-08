597163411e89e740f40313912eaedb0233a6602a
BorisMejia <borisfernandez916@gmail.com>
2023-11-07 20:45:25 -0500

    Actualizando entidades

--- src/main/java/com/example/konectaAPI/repositorios/ExamenRepositorio.java
+++ src/main/java/com/example/konectaAPI/repositorios/ExamenRepositorio.java
@@ -2,6 +2,8 @@ package com.example.konectaAPI.repositorios;
 
 import com.example.konectaAPI.entidades.Examen;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExamenRepositorio extends JpaRepository<Examen, Integer> {
 }
--- src/main/java/com/example/konectaAPI/servicios/Utilidad.java
+++ src/main/java/com/example/konectaAPI/servicios/Utilidad.java
@@ -2,7 +2,7 @@ package com.example.konectaAPI.servicios;
 
 public enum Utilidad
 {
-    ID_NEGATIVO("Error en el id care chimba porque es negativo"),
+    ID_NEGATIVO("Error en el id "),
     NOMBRES_LONGITUD("El numero de caracteres no es correcto debe estar entre 3 y 40"),
     NOMBRES_FORMATO("el nombre no coincide"),
     REGISTRAR_USUARIO("Error al registrar"),
